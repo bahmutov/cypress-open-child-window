33008dfb80d9d5a271dbc006d785edeb67877fe9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-09-03 22:12:36 -0400

    working test

---
+++ README.md
@@ -0,0 +1,7 @@
+# cypress-open-child-window
+
+Example using `window.open` to "pop" application and test it from Cypress test runner. This allows `top === self` and bypasses what many windows do to frame-bust, see [cypress/issues/886](https://github.com/cypress-io/cypress/issues/886).
+
+See [cypress/integration/spec.js](cypress/integration/spec.js), but basically there is a custom command `cy.openWindow` that uses `window.open` to create a child window linked back synchronously to the opener window. This still lets Cypress work.
+
+![Child window](images/child-window.png)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,24 +2,41 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
 
-context('Example Cypress TodoMVC test', () => {
-  beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
-    cy.visit('http://todomvc.com/examples/vue/')
-  })
+const normalVisit = () => cy.visit('index.html')
 
-  it('adds 2 todos', function () {
-    cy.get('.new-todo')
-      .type('learn testing{enter}')
-      .type('be cool{enter}')
-    cy.get('.todo-list li').should('have.length', 2)
-  })
+Cypress.Commands.add('openWindow', (url, features) => {
+  const w = Cypress.config('viewportWidth')
+  const h = Cypress.config('viewportHeight')
+  if (!features) {
+    features = `width=${w}, height=${h}`
+  }
+  console.log('openWindow %s "%s"', url, features)
 
-  // more examples
-  //
-  // https://github.com/cypress-io/cypress-example-todomvc
-  // https://github.com/cypress-io/cypress-example-kitchensink
-  // https://on.cypress.io/writing-your-first-test
+  return new Promise(resolve => {
+    if (window.top.aut) {
+      console.log('window exists already')
+      window.top.aut.close()
+    }
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+    window.top.aut = window.top.open(url, 'aut', features)
+
+    // letting page enough time to load and set "document.domain = localhost"
+    // so we can access it
+    setTimeout(() => {
+      cy.state('document', window.top.aut.document)
+      cy.state('window', window.top.aut)
+      resolve()
+    }, 500)
+  })
+})
+
+it('is frame busted', () => {
+  // cy.visit('/')
+  cy.openWindow('/')
+  cy.contains('Page body')
+
+  cy.get('button')
+    .click()
+    .click()
+  cy.get('#clicked').should('have.text', '2')
 })
